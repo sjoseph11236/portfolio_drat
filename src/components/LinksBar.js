99137353b2d623e612e7eadb3efc2cc33cf36e01
Sayeed Joseph <sayeedjoseph@gmail.com>
2019-12-23 19:33:02 -0500

    chore: add padding around icons

--- src/components/LinksBar.js
+++ src/components/LinksBar.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 const LinksBar = () => {
   return ( 
-    <div className="linksBar">
-      <a href="#" className="home"><i className="fa fa-home"></i></a>
-      <a href="https://www.linkedin.com/in/sayeed-joseph-3b95b9179/"><i class="fa fa-linkedin"></i></a>
-      <a href="https://github.com/sjoseph11236"><i class="fa fa-github"></i></a>
-      <a href="#"><i class="fa fa-envelope"></i></a>
+    <div>
+      <div className="linksBar">
+        <a href="#" className="home"><i className="fa fa-home"></i></a>
+        <a href="https://www.linkedin.com/in/sayeed-joseph-3b95b9179/"><i class="fa fa-linkedin"></i></a>
+        <a href="https://github.com/sjoseph11236"><i class="fa fa-github"></i></a>
+        <a href="#"><i class="fa fa-envelope"></i></a>
+      </div>
     </div>
+
   );
 }
 
