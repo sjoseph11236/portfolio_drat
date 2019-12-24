a83d3be7488265cb73250a896509edde9adea968
Sayeed Joseph <sayeedjoseph@gmail.com>
2019-12-23 19:04:37 -0500

    chore: add github and linkedin links

--- src/components/LinksBar.js
+++ src/components/LinksBar.js
@@ -4,9 +4,8 @@ const LinksBar = () => {
   return ( 
     <div className="linksBar">
       <a href="" className="home"><i className="fa fa-home"></i></a>
-      <a href="https://www.facebook.com/sammy.sasenarine"><i class="fa fa-facebook-square"></i></a>
-      <a href="https://www.instagram.com/samiam_gy/?hl=en"><i class="fa fa-github"></i></a>
-      <a href="#"><i class="fa fa-search"></i></a>
+      <a href="https://www.linkedin.com/in/sayeed-joseph-3b95b9179/"><i class="fa fa-linkedin"></i></a>
+      <a href="https://github.com/sjoseph11236"><i class="fa fa-github"></i></a>
       <a href="#"><i class="fa fa-envelope"></i></a>
     </div>
   );
