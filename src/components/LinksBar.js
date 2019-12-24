ad0e49bd3032568ebaa9951c9feab2647c08b5e4
Sayeed Joseph <sayeedjoseph@gmail.com>
2019-12-23 19:41:47 -0500

    fix: remove subject

--- src/components/LinksBar.js
+++ src/components/LinksBar.js
@@ -7,7 +7,7 @@ const LinksBar = () => {
         <a href="http://localhost:3000" className="home"><i className="fa fa-home"></i></a>
         <a href="https://www.linkedin.com/in/sayeed-joseph-3b95b9179/"><i class="fa fa-linkedin"></i></a>
         <a href="https://github.com/sjoseph11236"><i class="fa fa-github"></i></a>
-        <a href="mailto:sayeeedjoseph@gmail.com?Subject=Hello%20again"><i class="fa fa-envelope"></i></a>
+        <a href="mailto:sayeeedjoseph@gmail.com?"><i class="fa fa-envelope"></i></a>
       </div>
     </div>
 
