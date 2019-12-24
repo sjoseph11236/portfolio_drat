f2e6364314c04feb07b86239fa15cae9563a6efe
Sayeed Joseph <sayeedjoseph@gmail.com>
2019-12-23 19:01:24 -0500

    feat: add git hub logo

--- src/components/LinksBar.js
+++ src/components/LinksBar.js
@@ -4,6 +4,10 @@ const LinksBar = () => {
   return ( 
     <div className="linksBar">
       <a href="" className="home"><i className="fa fa-home"></i></a>
+      <a href="https://www.facebook.com/sammy.sasenarine"><i class="fa fa-facebook-square"></i></a>
+      <a href="https://www.instagram.com/samiam_gy/?hl=en"><i class="fa fa-github"></i></a>
+      <a href="#"><i class="fa fa-search"></i></a>
+      <a href="#"><i class="fa fa-envelope"></i></a>
     </div>
   );
 }
