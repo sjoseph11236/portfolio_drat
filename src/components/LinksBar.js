a42574ae8f30fc2c8c9cd2e9886bb8ff041bcedf
Sayeed Joseph <sayeedjoseph@gmail.com>
2019-12-23 19:41:25 -0500

    feat: add link to email with subject

--- src/components/LinksBar.js
+++ src/components/LinksBar.js
@@ -4,10 +4,10 @@ const LinksBar = () => {
   return ( 
     <div>
       <div className="linksBar">
-        <a href="#" className="home"><i className="fa fa-home"></i></a>
+        <a href="http://localhost:3000" className="home"><i className="fa fa-home"></i></a>
         <a href="https://www.linkedin.com/in/sayeed-joseph-3b95b9179/"><i class="fa fa-linkedin"></i></a>
         <a href="https://github.com/sjoseph11236"><i class="fa fa-github"></i></a>
-        <a href="#"><i class="fa fa-envelope"></i></a>
+        <a href="mailto:sayeeedjoseph@gmail.com?Subject=Hello%20again"><i class="fa fa-envelope"></i></a>
       </div>
     </div>
 
